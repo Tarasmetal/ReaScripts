39e20180bc56a98c6a97b5b876dbd0f5a23ab582
Taras Umanskiy <Tarasmetal@gmail.com>
2022-06-27 01:03:01 +0300

    UPDATE

---
+++ GUI Tools/MarkerPresets/Short.lua
@@ -0,0 +1,20 @@
+-- @description MarkerGUITools Preset
+-- @author Taras Umanskiy
+-- @version 1.0
+-- @provides [nomain] .
+Int,
+Vrs,
+p_Chrs,
+Chrs,
+Brdg,
+BrkDwn,
+p_Drp,
+Drp,
+Silent,
+Outro,
+Riff,
+Idea,
+p_Beat,
+Beat,
+Fx,
+Phase,
---
+++ GUI Tools/MarkerPresets/Takes.lua
@@ -0,0 +1,9 @@
+-- @description MarkerGUITools Preset
+-- @author Taras Umanskiy
+-- @version 1.0
+-- @provides [nomain] .
+Nice,
+Good,
+Shit,
+Bad,
+Error,
