18d76b638f2ff29226d453694300f3af126a3039
Taras Umanskiy <Tarasmetal@gmail.com>
2025-12-11 22:58:14 +0300

    GUI Scripts and Presets

---
+++ GUI Tools/MarkerPresets/Default.lua
@@ -0,0 +1,24 @@
+-- @description MarkerGUITools Preset
+-- @author Taras Umanskiy
+-- @version 1.0
+-- @provides [nomain] .
+Intro,
+Main,
+Verse,
+Chorus,
+Fill,
+Pre,
+Bridge,
+BDown,
+Drop,
+Beat,
+Silent,
+Riff,
+Solo,
+Tap,
+Outro,
+Phase,
+Idea,
+Old,
+Fx,
+!1016,
